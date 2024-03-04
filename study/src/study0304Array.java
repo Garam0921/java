6b4ab60f5d007d4906e2f7b4a683144b99b0b8fa
Garam0921 <161662789+Garam0921@users.noreply.github.com>
2024-03-04 15:36:46 +0900

    change

--- study/src/study0304Array.java
+++ study/src/study0304Array.java
@@ -13,10 +13,30 @@ public class study0304Array {
         intArray2[4] = 5;
         System.out.println(intArray2[4]);
 
-        int[] intArray3 = new int[5];
-        for (int i=0; i<5; i++){
+        int[] intArray3 = new int[50];
+        // int[] intArray3 = 가변의 길이를 가진 배열을 저장함
+        for (int i=0; i<intArray3.length; i++){
             intArray3[i] = i+1;
             System.out.print(intArray3[i] + " ");
         }
+        System.out.println();
+
+        int[] intArray4 = new int[]{1,2,3,4,5};
+        System.out.println(intArray4[0]);
+
+        //배역의 복사 (= 참조복사)
+        String[] str1 = {"black", "red", "blue"};
+        String[] str2 = str1;
+        System.out.println("1" + str2[0]);
+        str2[0] = "white";
+        System.out.println("2" + str2[0]);
+        System.out.println("3" + str1[0]);
+        // 기본자료형의 복사 (= 값복사)
+        int a = 3;
+        int b = a;
+        System.out.println("1 " + b);
+        b = 6;
+        System.out.println("2 " + b);
+        System.out.println("3 " + a);
     }
 }
