2c4e0f9c5112f0010a3639b788e353e48ec2733e
Garam0921 <161662789+Garam0921@users.noreply.github.com>
2024-03-06 11:38:11 +0900

    change

---
+++ study/src/practice.java
@@ -0,0 +1,9 @@
+public class practice {
+    public static void main(String[] args) {
+        boolean[] array1 = new boolean[3];
+        for(int i = 0; i < 3; i++) {
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+    }
+}
