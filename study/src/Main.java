5122f27f4e86c7d4ad2da269149063a6393b0f80
Garam0921 <161662789+Garam0921@users.noreply.github.com>
2024-03-12 14:02:45 +0900

    change

--- study/src/Main.java
+++ study/src/Main.java
@@ -1,23 +1,17 @@
-package Main;
-
-class A {
-    int m = 3;
-    void print() {
-        System.out.println("객체 생성 및 활용");
-    }
-}
 public class Main {
-    public static void main(String[] args) {
-        // 클래스로 객체(붕어빵) 생성
-        A a = new A();
 
-        // 클래스 멤버 활용(붕어빵 먹기)
-        // @필드 활용: 필드에 값을 읽기/쓰기
-        a.m = 5;
-        System.out.println(a.m);
+    static int name = 10000;
 
-        // @메서드 활용: 메서드릴 호출
-        a.print();
+    int age;
+
+    static {
+        name = 10000;
     }
 
+    public Main() {
+    }
+
+    public Main(int age) {
+        this.age = age;
+    }
 }
---
+++ study/src/study0312.java
@@ -0,0 +1,38 @@
+/*
+package sec01_accessmodifier.EX02_AccessModifierOfClass_1;
+
+*/
+/*같은 패키지에서의 클래스 접근지정자 vs. 생성자 접근지정자*//*
+
+
+//public class
+public class A {
+    //public 기본생성자를 컴파일러가 추가
+}
+
+
+
+
+*/
+/*같은 패키지에서의 클래스 접근지정자 vs. 생성자 접근지정자*//*
+
+
+//default class
+class B {
+    //default 생성자 자동 추가
+}
+
+
+
+
+*/
+/*같은 패키지에서의 클래스 접근지정자 vs. 생성자 접근지정자*//*
+
+
+//public class
+public class C {
+    C(){ //default 생성자
+
+    }
+}
+*/
